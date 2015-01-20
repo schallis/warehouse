c7e7020a5f26e84fffd0a81b08702b0ed77882bb
Steven Challis <steven.challis@hogarthww.com>
2015-01-20 15:27:55 -0500

    Fix Django 1.7 issues

--- warehouse/urls.py
+++ warehouse/urls.py
@@ -1,4 +1,4 @@
-from django.conf.urls.defaults import patterns, include, url
+from django.conf.urls import patterns, include, url
 
 from django.contrib import admin
 admin.autodiscover()
@@ -6,10 +6,6 @@ admin.autodiscover()
 from reporting import urls as reporting_urls
 
 urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'vidiwarehouse.views.home', name='home'),
-    # url(r'^vidiwarehouse/', include('vidiwarehouse.foo.urls')),
-
     url(r'^admin/', include(admin.site.urls)),
     url(r'^', include(reporting_urls)),
 )
