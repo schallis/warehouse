c5c974d4796f08caf4e487e788af072de9e6ecd7
Steven Challis <steven.challis@hogarthww.com>
2015-02-10 03:03:09 -0500

    Remove Celery

--- warehouse/reporting/tasks.py
+++ warehouse/reporting/tasks.py
@@ -1,8 +1,8 @@
 import logging
 
-from celery.task import PeriodicTask
-from celery.schedules import crontab
-from celery.registry import tasks
+#from celery.task import PeriodicTask
+#from celery.schedules import crontab
+#from celery.registry import tasks
 
 from django.utils import timezone
 
@@ -13,31 +13,31 @@ log = logging.getLogger(__name__)
 PERCENTILES = 100
 
 
-class SyncReportDataTask(PeriodicTask):
-    """Called asynchronously by CeleryBeat Scheduler"""
+#class SyncReportDataTask(PeriodicTask):
+    #"""Called asynchronously by CeleryBeat Scheduler"""
 
-    name = "reporting.tasks.SyncReportDataTask"
-    routing_key = "celery"
-    run_every = crontab(hour="4")
+    #name = "reporting.tasks.SyncReportDataTask"
+    #routing_key = "celery"
+    #run_every = crontab(hour="4")
 
-    def run(self, hours=24, **kwargs):
+    #def run(self, hours=24, **kwargs):
 
-        log.debug('SyncReportDataTask: Started at {0}'.format(
-                timezone.now().isoformat()))
+        #log.debug('SyncReportDataTask: Started at {0}'.format(
+                #timezone.now().isoformat()))
 
-        objects = Asset.vidispine_objects.all()
-        count = objects.count()
-        done = 0
+        #objects = Asset.vidispine_objects.all()
+        #count = objects.count()
+        #done = 0
 
-        for item in objects:
-            done += 1
-            self.update_progress(done, count)
+        #for item in objects:
+            #done += 1
+            #self.update_progress(done, count)
 
-    def update_progress(self, done, count):
-        percentiles = [int(count * percentile / PERCENTILES)
-                       for percentile in range(0, PERCENTILES + 1)]
-        if done in percentiles:
-            log.debug('SyncReportDataTask: {0}% ({1}/{2})'.format(
-                    int(PERCENTILES * done / count), done, count))
+    #def update_progress(self, done, count):
+        #percentiles = [int(count * percentile / PERCENTILES)
+                       #for percentile in range(0, PERCENTILES + 1)]
+        #if done in percentiles:
+            #log.debug('SyncReportDataTask: {0}% ({1}/{2})'.format(
+                    #int(PERCENTILES * done / count), done, count))
 
-tasks.register(SyncReportDataTask)
+#tasks.register(SyncReportDataTask)
--- warehouse/reporting/views.py
+++ warehouse/reporting/views.py
@@ -1,5 +1,3 @@
-from celery.task.control import inspect
-
 from django.contrib.admin.views.decorators import staff_member_required
 from django.contrib import messages
 from django.shortcuts import render, redirect
